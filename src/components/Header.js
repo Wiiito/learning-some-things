5a9b146b552746fa554e1427b6e59015060a13b4
Wiito <tminemcraft123@gmail.com>
2021-03-18 21:20:09 -0300

    Header Done

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,85 @@
-import React from 'react'
+import React from 'react';
+import logo from '../img/logo.svg';
+import avatar from '../img/avatar.svg';
+import '../css/Header.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBell, faSearch } from '@fortawesome/free-solid-svg-icons'
+
+
+var notifications = [
+    {
+        "name":"WOW",
+        "desc":"desc",
+        "img":"src"
+    },
+    {
+        "name":"WOW",
+        "desc":"desc",
+        "img":"src"
+    },
+    {
+        "name":"WOW",
+        "desc":"desc",
+        "img":"src"
+    },
+    {
+        "name":"WOW",
+        "desc":"desc",
+        "img":"src"
+    },
+    {
+        "name":"WOW",
+        "desc":"desc",
+        "img":"src"
+    },
+];
+var notificationsNumber = notifications.length;
 
 const Header = () => {
     return (
-        <header>
-            <h1>Hello World!</h1>
+        <header className='flex jc-sb'>
+            <nav className='flex jc-sb'>
+                <div className='logo-div'>
+                    <a href="/"><img src={logo} className='tran-2'/></a>
+                </div>
+                <ul className='links-ul no-dec flex ai-c'>
+                    <li className='pd-10'><a href='/' className='no-dec tran-2'>Home</a></li>
+                    <li className='pd-10'><a href='/' className='no-dec tran-2'>TV Shows</a></li>
+                    <li className='pd-10'><a href='/' className='no-dec tran-2'>Movies</a></li>
+                    <li className='pd-10'><a href='/' className='no-dec tran-2'>Recently Added</a></li>
+                    <li className='pd-10'><a href='/' className='no-dec tran-2'>My List</a></li>
+                </ul>
+            </nav>
+            <div className='menu-right flex ai-c'>
+                <button className='search-btn btn mh-10 tran-2'>
+                    <FontAwesomeIcon icon={faSearch}/>
+                </button>
+                <h4 className='mh-10 tran-2'>children</h4>
+                <button className='bell-btn btn mh-10 tran-2'>
+                    <FontAwesomeIcon icon={faBell} />
+                    {note()}
+                </button>
+                <div className='avatar-cont mh-10'>
+                    <div className='point avatar tran-2'>
+                        <img src={avatar} />
+                    </div>
+                </div>
+            </div>
         </header>
     )
 }
+
+function note() {
+    if (notificationsNumber > 0) {
+        if (notificationsNumber > 9) {
+            return <span className='notification-n flex ai-c jc-c more9 tran-2'>+9</span>
+        } else {
+            return (
+                <span className='notification-n flex ai-c jc-c font-16 tran-2'>{notificationsNumber}</span>
+            )
+        }
+    }
+}
+
 
 export default Header
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom';
 import App from './App';
-import './css/index.css'
+import './css/index.css';
+import './css/globals.css'
 
 ReactDOM.render(
   <React.StrictMode>
